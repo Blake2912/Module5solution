7b3ec2bfa85d005734c1c0c9a49dc687d89a7f78
Blake2912 <kcvarun29122001@gmail.com>
2020-09-15 17:36:49 +0530

    commit

--- Module5solution/js/script.js
+++ Module5solution/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      chosenCategoryShortName = chosenCategoryShortName.short_name
+      chosenCategoryShortName = chosenCategoryShortName.short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
